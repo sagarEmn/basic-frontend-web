d2286cadce66562240772dff6af5e24b06c5ecd4
Sagar <jussagar1@gmail.com>
2024-10-09 21:52:46 +0545

    Access, store references to DOM elements

---
+++ 1 - Todo App/main.js
@@ -0,0 +1,12 @@
+// form
+const form = document.querySelector('form');
+
+// input text box
+const todoInput = document.getElementById('todo-input');
+
+// add button
+const addButton = document.getElementById('add-button');
+
+// todo list ul
+const todoList = document.getElementById('todo-app__list');
+
