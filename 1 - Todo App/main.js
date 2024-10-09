0b6c6df391b19101750e2bc5879468339a46f5a8
Sagar <jussagar1@gmail.com>
2024-10-09 23:15:26 +0545

    Fix: todoId bug

--- 1 - Todo App/main.js
+++ 1 - Todo App/main.js
@@ -27,12 +27,11 @@ function handleFormSubmit(event) {
 
 // create new todo item
 function createTodoItem(todoText) {
-  const todoId = "todo-" + Date.now();
+  const todoId = "todo-" + crypto.randomUUID();
   const todoItem = document.createElement("li");
   todoItem.className = "todo__item";
-  todoItem.innerHTML = `
-     <input type="checkbox" id="todo-1" />
-            <label class="todo__item__checkbox" for="todo-1">
+  todoItem.innerHTML = `  <input type="checkbox" id="${todoId}" />
+            <label class="todo__item__checkbox" for="${todoId}">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
@@ -45,8 +44,8 @@ function createTodoItem(todoText) {
                 />
               </svg>
             </label>
-            <label for="todo-1" class="todo__item__text"> ${todoText} </label>
-            <label for="todo-1" class="todo__item__delete">
+            <label for="${todoId}" class="todo__item__text"> ${todoText} </label>
+            <label for="${todoId}" class="todo__item__delete">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="24px"
@@ -58,8 +57,7 @@ function createTodoItem(todoText) {
                   d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
                 />
               </svg>
-            </label>
-    `;
+            </label>`;
 
   // handle checking/unchecking todo item
   const checkbox = todoItem.querySelector('input[type="checkbox"]');
