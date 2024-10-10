cce5a8af489dc7bfc419e44a8a94fa32ed1e62c8
Sagar <jussagar1@gmail.com>
2024-10-10 07:51:59 +0545

    Minor changes

--- 1 - Todo App/main.js
+++ 1 - Todo App/main.js
@@ -30,7 +30,8 @@ function createTodoItem(todoText) {
   const todoId = "todo-" + crypto.randomUUID();
   const todoItem = document.createElement("li");
   todoItem.className = "todo__item";
-  todoItem.innerHTML = `  <input type="checkbox" id="${todoId}" />
+  todoItem.innerHTML = `
+            <input type="checkbox" id="${todoId}" />
             <label class="todo__item__checkbox" for="${todoId}">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +58,8 @@ function createTodoItem(todoText) {
                   d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
                 />
               </svg>
-            </label>`;
+            </label>
+            `;
 
   // handle checking/unchecking todo item
   const checkbox = todoItem.querySelector('input[type="checkbox"]');
