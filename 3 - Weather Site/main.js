cdcb12fb52174eac15d80a3d2c3bd04a77ddc3a1
Sagar <desiferchef@gmail.com>
2024-11-16 11:39:23 +0545

    Add weather api key

--- 3 - Weather Site/main.js
+++ 3 - Weather Site/main.js
@@ -2,3 +2,8 @@ const searchBox = document.querySelector(".search-box input");
 const searchBtn = document.querySelector("#search");
 const weatherIcon = document.querySelector(".weather-icon");
 const appContainer = document.querySelector(".app-container");
+
+// weather api
+const weatherApiKey = 'b745dd26b8cfa0799abbd529b98b4286';
+const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901`;
+
