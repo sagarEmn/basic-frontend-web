98d3d135c5d3168e7fc1675427b51dc61cd8d487
Sagar <desiferchef@gmail.com>
2024-11-16 11:40:46 +0545

    Add preloader display none timeout

--- 3 - Weather Site/main.js
+++ 3 - Weather Site/main.js
@@ -4,6 +4,11 @@ const weatherIcon = document.querySelector(".weather-icon");
 const appContainer = document.querySelector(".app-container");
 
 // weather api
-const weatherApiKey = 'b745dd26b8cfa0799abbd529b98b4286';
+const weatherApiKey = "b745dd26b8cfa0799abbd529b98b4286";
 const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901`;
 
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.querySelector("#preloader").style.display = "none";
+  }, 1500);
+});
