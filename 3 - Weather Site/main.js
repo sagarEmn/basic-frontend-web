1d3a6565d53950480a3fa5ffad0af54412b7a4e2
Sagar <desiferchef@gmail.com>
2024-11-16 11:37:18 +0545

    Access basic html elements

---
+++ 3 - Weather Site/main.js
@@ -0,0 +1,4 @@
+const searchBox = document.querySelector(".search-box input");
+const searchBtn = document.querySelector("#search");
+const weatherIcon = document.querySelector(".weather-icon");
+const appContainer = document.querySelector(".app-container");
